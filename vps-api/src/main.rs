68814e098a96f36a96471f3f12e645424f35796d
Othi <mnpq.raven@gmail.com>
2025-02-23 14:07:43 +0700

    chore

--- vps-api/src/main.rs
+++ vps-api/src/main.rs
@@ -1,6 +1,7 @@
 pub mod routes;
 pub mod handler;
 use routes::app_router;
+use tracing::info;
 
 pub const API_ADDR: &str = "[::1]:4000";
 
@@ -12,6 +13,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind(API_ADDR).await.unwrap();
+    info!("{} API SERVER UP", API_ADDR);
 
     axum::serve(listener, app).await?;
 
