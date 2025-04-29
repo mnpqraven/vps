f2e0b0ed7292c5ed4c5481cf21c6fff102f88616
Othi <mnpq.raven@gmail.com>
2025-04-29 13:40:08 +0700

    feat(flake):

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "VPS control tools";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+      in
+      {
+
+        # nix develop
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            rustc
+            cargo
+            bacon
+          ];
+        };
+      }
+    );
+}
