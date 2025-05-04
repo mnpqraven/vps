13b5d078355970c453efedf8e213ffdfd1e23bc6
Othi <mnpq.raven@gmail.com>
2025-05-04 22:25:44 +0700

    chore

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
         };
         rpcWeb = pkgs.writeShellScriptBin "rpcWeb" ''
           PORT=5005
-           ${pkgs.grpcui}/bin/grpcui -plaintext localhost:$PORT
+          ${pkgs.grpcui}/bin/grpcui -plaintext localhost:$PORT || echo "is the gRPC server running on port $PORT ?"
         '';
       in
       {
