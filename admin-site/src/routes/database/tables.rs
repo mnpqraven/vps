906dd8b8739d9b84db619ac1c04d41bb4f7f015c
Othi <mnpq.raven@gmail.com>
2025-06-02 17:29:00 +0700

    chore: initial ssr logic(err)

--- admin-site/src/routes/database/tables.rs
+++ admin-site/src/routes/database/tables.rs
@@ -1,8 +1,39 @@
 use leptos::prelude::*;
 
+#[cfg(feature = "ssr")]
 #[component]
 pub fn DatabaseTablesPage() -> impl IntoView {
-    // TODO: dynamic params
+    use proto_types::{
+        blog::tag::{
+            blog_tag_service_client::BlogTagServiceClient, BlogTag, BlogTagList, BlogTagShape,
+        },
+        common::db::Pagination,
+    };
 
-    view! { <div>"hello DatabaseTablesPage"</div> }
+    // TODO: dynamic params
+    let (pagination, set_pagination) = signal(Pagination::default());
+
+    // TODO: type or use #[server]
+    let async_data = Resource::new(
+        // TODO: unwrap
+        move || pagination.get(),
+        |_pagination| async {
+            // TODO: unwrap
+            let client = BlogTagServiceClient::connect("grpc://127.0.0.1:5005")
+                .await
+                .unwrap();
+            // TODO: unwrap
+            let res = client.list(_pagination).await.unwrap();
+
+            Ok(res.into_inner())
+        },
+    );
+
+    view! { <div>"hello DatabaseTablesPage (ssr)"</div> }
+}
+
+#[cfg(not(feature = "ssr"))]
+#[component]
+pub fn DatabaseTablesPage() -> impl IntoView {
+    view! { <div>"hello DatabaseTablesPage (non ssr)"</div> }
 }
