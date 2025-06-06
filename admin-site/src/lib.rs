a5f6bb8cb7f811eda53413e7485eb36bf0895c72
Othi <mnpq.raven@gmail.com>
2025-06-06 14:36:23 +0700

    generic table view

--- admin-site/src/lib.rs
+++ admin-site/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod app;
 pub mod routes;
 pub mod state;
+pub mod ui;
 
 #[cfg(feature = "hydrate")]
 #[wasm_bindgen::prelude::wasm_bindgen]
--- admin-site/src/routes/database/tables.rs
+++ admin-site/src/routes/database/tables.rs
@@ -1,46 +1,30 @@
+use crate::ui::primitive::table::{ColumnDefs, Table};
 use leptos::prelude::*;
-use proto_types::{blog::tag::BlogTagList, common::db::Pagination, impls::DefaultState};
+use proto_types::{
+    blog::tag::{BlogTag, BlogTagList},
+    common::db::Pagination,
+    impls::DefaultState,
+};
 
 #[component]
 pub fn DatabaseTablesPage() -> impl IntoView {
     // TODO: dynamic params
     let (pagination, set_pagination) = signal(Pagination::default_state());
 
-    let async_data = Resource::new(move || pagination.get(), load);
+    let async_data = Resource::new(move || pagination.get(), get_blog_tags);
+
+    let defs = ColumnDefs::<BlogTag>::new()
+        .col("ID", |row| row.id.clone().into_any())
+        .col("Code", |row| row.code.clone().into_any())
+        .col("Label", |row| row.label.clone().into_any());
 
     let table_view = move || {
-        async_data.get().map(|data| {
-            let data = data.unwrap();
-            view! { <BlogTagTable data pagination set_pagination /> }
+        async_data.get().map(|result| {
+            let req = result.unwrap();
+            let defs = defs.clone();
+            view! { <Table data={req.data} column_defs=defs /> }
         })
     };
-
-    view! {
-        <div class="flex flex-col gap-4 p-4">
-            <Suspense fallback=move || view! { <p>"Loading..."</p> }>{table_view}</Suspense>
-        </div>
-    }
-}
-
-#[component]
-fn BlogTagTable(
-    data: BlogTagList,
-    pagination: ReadSignal<Pagination>,
-    set_pagination: WriteSignal<Pagination>,
-) -> impl IntoView {
-    let row_views = data
-        .data
-        .into_iter()
-        .map(|row| {
-            view! {
-                <tr>
-                    <td>{row.id}</td>
-                    <td>{row.code}</td>
-                    <td>{row.label}</td>
-                </tr>
-            }
-        })
-        .collect_view();
 
     let on_prev = move |_| {
         set_pagination.update(|prev| {
@@ -52,17 +36,8 @@ fn BlogTagTable(
     let on_next = move |_| set_pagination.update(|prev| prev.page_index += 1);
 
     view! {
-        <div class="flex flex-col gap-4">
-            <table class="border">
-                <tbody>
-                    <tr class="border border-b">
-                        <th>"ID"</th>
-                        <th>"Code"</th>
-                        <th>"Label"</th>
-                    </tr>
-                    {row_views}
-                </tbody>
-            </table>
+        <div class="flex flex-col gap-4 p-4">
+            <Suspense fallback=move || view! { <p>"Loading..."</p> }>{table_view}</Suspense>
 
             <div class="flex gap-2 items-center">
                 <button on:click=on_prev>"prev"</button>
@@ -74,7 +49,7 @@ fn BlogTagTable(
 }
 
 #[server]
-async fn load(pagination: Pagination) -> Result<BlogTagList, ServerFnError> {
+async fn get_blog_tags(pagination: Pagination) -> Result<BlogTagList, ServerFnError> {
     use crate::state::ctx;
     use proto_types::blog::tag::blog_tag_service_client::BlogTagServiceClient;
 
