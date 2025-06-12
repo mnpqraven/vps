e69b9fbfa81eb5f333c515b683c27369af068f48
Othi <mnpq.raven@gmail.com>
2025-06-12 11:11:19 +0700

    feat(database): unique constraint

--- database/migrations/20250612040937_init.up.sql
+++ database/migrations/20250612040937_init.up.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS blog_tag CASCADE;
 CREATE TABLE blog_tag (
     id VARCHAR(50) PRIMARY KEY,
     code VARCHAR(255) UNIQUE NOT NULL,
-    label VARCHAR(255) NOT NULL,
+    label VARCHAR(255) NOT NULL UNIQUE,
     created_at BIGINT NOT NULL,
     updated_at BIGINT NOT NULL
 );
@@ -13,7 +13,7 @@ DROP TABLE IF EXISTS blog_meta CASCADE;
 CREATE TABLE blog_meta (
     id VARCHAR(50) PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    file_name VARCHAR(255) NOT NULL,
+    file_name VARCHAR(255) NOT NULL UNIQUE,
     is_publish BOOLEAN NOT NULL DEFAULT false,
     created_at BIGINT NOT NULL,
     updated_at BIGINT NOT NULL
