d6a32606c5783e6658fe841614640cc45184a49b
Othi <mnpq.raven@gmail.com>
2025-07-18 13:28:11 +0700

    chore

--- admin-site/src/routes/database/tables/blog/create.rs
+++ admin-site/src/routes/database/tables/blog/create.rs
@@ -40,10 +40,9 @@ pub fn MetaForm() -> impl IntoView {
             <pre>{error}</pre>
 
             <ActionForm action>
-                // TODO: tag selector
                 <div class="flex flex-col gap-4 w-fit items-start">
                     <FormInput label="Title" field="title" />
-                    <MultiCheckbox />
+                    <TagsSelector />
                     <FormTextarea label="Content" field="content" />
                     <FormCheckbox label="Publish" field="is_publish" />
 
@@ -58,7 +57,7 @@ pub fn MetaForm() -> impl IntoView {
 async fn create_blog(
     title: String,
     content: String,
-    tag_ids: Vec<String>,
+    #[server(default)] tag_ids: Vec<String>,
     #[server(default)] is_publish: bool,
 ) -> Result<(), ServerFnError> {
     use crate::state::ctx;
@@ -66,33 +65,28 @@ async fn create_blog(
     use proto_types::blog::meta::BlogMetaShape;
     use proto_types::blog::root::{BlogShape, blog_service_client::BlogServiceClient};
 
-    let _rpc = BlogServiceClient::connect(ctx()?.rpc_url).await?;
+    let mut rpc = BlogServiceClient::connect(ctx()?.rpc_url).await?;
 
-    // TODO: get filename from title (hypenized)
-    let file_name = String::from("frontend_placeholder.md");
-
-    let payload: BlogShape = BlogShape {
+    let payload = BlogShape {
         meta_shape: Some(BlogMetaShape {
-            title,
-            file_name,
+            title: title.clone(),
+            file_name: hyphen_filename(&title),
             is_publish,
         }),
         tag_ids, // TODO:
         file_content: content,
     };
 
-    leptos::logging::log!("{payload:?}");
-    tracing::info!("{payload:?}");
-
-    // let _ = rpc.create(payload).await?;
+    rpc.create(payload).await?;
+    // TODO: media upload here
 
     leptos_axum::redirect(RouterKey::DatabaseTablesBlog.as_ref());
+
     Ok(())
 }
 
-// TODO: serde error
 #[component]
-fn MultiCheckbox() -> impl IntoView {
+fn TagsSelector() -> impl IntoView {
     let async_data = Resource::new(
         move || (),
         |_| {
@@ -104,14 +98,17 @@ fn MultiCheckbox() -> impl IntoView {
         },
     );
 
-    let suspend_views = move || {
+    let tag_checkbox_views = move || {
         async_data.get().map(|result| {
+            // TODO: unwrap
             let tags = result.unwrap().data;
             tags.into_iter()
-                .map(|tag| {
+                .enumerate()
+                .map(|(i, tag)| {
+                    let name = format!("tag_ids[{i}]");
                     view! {
                         <div>
-                            <input type="checkbox" name="tag_ids" value=tag.id />
+                            <input type="checkbox" name=name value=tag.id />
                             <span>{tag.label}</span>
                         </div>
                     }
@@ -123,7 +120,18 @@ fn MultiCheckbox() -> impl IntoView {
     view! {
         <fieldset>
             <legend>"Tags"</legend>
-            <Transition>{suspend_views}</Transition>
+            <Transition>{tag_checkbox_views}</Transition>
         </fieldset>
     }
+}
+
+fn hyphen_filename(filename: &str) -> String {
+    use std::time::SystemTime;
+
+    let now = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+    let slug = filename.to_lowercase().replace(" ", "-");
+    format!("{now}_{slug}.md")
 }
