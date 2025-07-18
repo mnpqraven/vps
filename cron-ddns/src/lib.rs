cdf260f50bd89b6e5ba6ad14a6c2f6cdb93cc7d4
Othi <mnpq.raven@gmail.com>
2025-07-18 10:22:46 +0700

    initial cron logic without cf api calls

--- cron-ddns/src/lib.rs
+++ cron-ddns/src/lib.rs
@@ -1,4 +1,7 @@
+use crate::utils::error::CronDdnsError;
 use std::process::Command;
+
+pub mod utils;
 
 fn public_ip() -> String {
     // dig -6 TXT +short o-o.myaddr.l.google.com @ns1.google.com
@@ -12,16 +15,15 @@ fn public_ip() -> String {
         ])
         .output();
     let result = cmd.expect("is dig installed ?").stdout;
-    let ip = String::from_utf8(result)
+    String::from_utf8(result)
         .expect("command output should product normal utf8 characters")
         .trim()
-        .replace("\"", "");
-    ip
+        .replace("\"", "")
 }
 
-pub async fn update_cf_conf() {
+pub async fn update_cf_conf() -> Result<(), CronDdnsError> {
     let _ip = public_ip();
-    todo!("CALLING CF UPDATE API");
+    Ok(())
 }
 
 #[cfg(test)]
--- cron-ddns/src/main.rs
+++ cron-ddns/src/main.rs
@@ -1,20 +1,25 @@
 use apalis::prelude::*;
 use apalis_cron::{CronContext, CronStream, Schedule};
 use chrono::Local;
+use cron_ddns::{update_cf_conf, utils::error::CronDdnsError};
 use std::str::FromStr;
 use tower::load_shed::LoadShedLayer;
-use tracing::info;
+use tracing::{info, instrument};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<(), CronDdnsError> {
     tracing_subscriber::fmt::init();
 
     // https://www.freetool.dev/crontab-generator/
-    let schedule = Schedule::from_str("* * * * * *")?;
+    let schedule = "* * * * * *";
+
+    let schedule = Schedule::from_str(schedule)
+        .map_err(|_e| CronDdnsError::Unknown("Bad cron string {schedule}".into()))?;
 
     let worker = WorkerBuilder::new("cron-ddns")
         .enable_tracing()
-        .layer(LoadShedLayer::new()) // Important when you have layers that block the service
+        // Important when you have layers that block the service
+        .layer(LoadShedLayer::new())
         .backend(CronStream::new_with_timezone(schedule, Local))
         .build_fn(send_reminder);
     worker.run().await;
@@ -25,7 +30,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 #[derive(Debug, Default)]
 struct Reminder;
 
-async fn send_reminder(_job: Reminder, ctx: CronContext<Local>) {
+#[instrument(ret)]
+async fn send_reminder(_job: Reminder, ctx: CronContext<Local>) -> Result<(), CronDdnsError> {
     let time = ctx.get_timestamp();
-    info!("[CRON] timestamp: {}", time)
+    info!("[CRON] timestamp: {}", time);
+    update_cf_conf().await?;
+    Err(CronDdnsError::Unknown("WIP".into()))
 }
