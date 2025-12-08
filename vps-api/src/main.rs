d74c47c36701a820e562b5e6e2604c42f87f387b
Othi <mnpq.raven@gmail.com>
2025-12-09 00:41:16 +0700

    add cors for local development + fix prost build

--- vps-api/src/main.rs
+++ vps-api/src/main.rs
@@ -1,4 +1,5 @@
 use crate::routes::make_app_router;
+use tower_http::cors::{Any, CorsLayer};
 use tracing::info;
 
 pub mod routes;
@@ -10,7 +11,18 @@ pub const API_ADDR: &str = "127.0.0.1:5000";
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt::init();
 
-    let app_router = make_app_router();
+    let cors = CorsLayer::new()
+        .allow_methods(Any)
+        .allow_headers(Any)
+        .allow_origin([
+            // local web ui
+            "http://127.0.0.1:3000".parse().unwrap(),
+            "http://127.0.0.1:4004".parse().unwrap(),
+            "http://localhost:3000".parse().unwrap(),
+            "http://localhost:4004".parse().unwrap(),
+            // prod origins
+        ]);
+    let app_router = make_app_router().layer(cors);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind(API_ADDR).await.unwrap();
